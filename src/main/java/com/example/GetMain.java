b041a30d5d49eb2068b4c7041ec7148037215f18
João Schneider <joaoguischneiders@gmail.com>
2024-12-07 17:25:12 -0300

    adicionado cdimascio, para controle de api ket pelo .env

--- src/main/java/com/example/GetMain.java
+++ src/main/java/com/example/GetMain.java
@@ -7,28 +7,35 @@ import java.net.http.HttpResponse;
 
 import org.json.JSONObject;
 
-public class GetMain {
-    static String escolhaFilme =  "avengers";
-    static String OMDB_KEY = "";
+import io.github.cdimascio.dotenv.Dotenv;
 
+public class GetMain {
+    static String escolhaFilme = "avengers";
+    
     public static void main(String[] args) {
         try {
-            HttpClient client = HttpClient.newHttpClient();
+            Dotenv dotenv = Dotenv.load();
+            String OMDB_KEY = dotenv.get("OMDB_KEY");
             
+            if (OMDB_KEY == null) {
+                System.out.println("API Key não encontrada no .env");
+                return;
+            }
+            
+            HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://www.omdbapi.com/?t=" + escolhaFilme + "&apikey=" + OMDB_KEY))
                 .build();
-        
+
             client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(response -> {
-                    
                     JSONObject json = new JSONObject(response);
                     if (json.getString("Response").equals("True")) {
                         String title = json.getString("Title");
                         String year = json.getString("Year");
                         String director = json.getString("Director");
-        
+
                         System.out.println("Title: " + title);
                         System.out.println("Year: " + year);
                         System.out.println("Director: " + director);
@@ -41,6 +48,5 @@ public class GetMain {
             System.out.println("Erro ao buscar filme na API: " + e.getMessage());
             e.printStackTrace();
         }
-        
     }
 }
